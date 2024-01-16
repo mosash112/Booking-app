cb35222ab7474a029d9607ac981a64264d1ee97f
mosash <mashalex2000@gmail.com>
2024-01-16 08:45:18 +0200

    added my hotels view and added e2e test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Register from './pages/Register';
 import SignIn from './pages/SignIn';
 import AddHotel from './pages/AddHotel';
 import { useAppContext } from './contexts/AppContext';
+import MyHotels from './pages/MyHotels';
 
 const App = () => {
   const { isLoggedIn } = useAppContext()
@@ -37,7 +38,11 @@ const App = () => {
               <AddHotel />
             </Layout>
           } />
-
+          <Route path="/my-hotels" element={
+            <Layout>
+              <MyHotels />
+            </Layout>
+          } />
         </>}
         <Route path="*" element={<Navigate to="/"></Navigate>} />
       </Routes>
