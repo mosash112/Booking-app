1673f501578be18aa5e93653fc86c2794b7cb402
mosash <mashalex2000@gmail.com>
2024-01-16 11:10:58 +0200

    secure url for the images

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -93,7 +93,7 @@ async function uploadImages(imageFiles: Express.Multer.File[]) {
         const b64 = Buffer.from(image.buffer).toString('base64')
         let dataURI = 'data:' + image.mimetype + ';base64,' + b64
         const res = await cloudinary.uploader.upload(dataURI)
-        return res.url
+        return res.secure_url
     })
 
     const imageUrls = await Promise.all(uploadPromises)
